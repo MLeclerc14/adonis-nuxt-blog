d0e528a6f4407f734fa501cdd5db87d6d11f1232
Maxime LECLERC <contact@maxime-leclerc.com>
2018-01-19 18:17:35 +0100

    feat(bootstrap): add bootstrap / bootstrap-vue

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -14,6 +14,15 @@ module.exports = {
     ]
   },
   /*
+  ** List of modules
+  */
+  modules: [
+    'bootstrap-vue/nuxt',
+
+    // Or if you have custom bootstrap CSS...
+    ['bootstrap-vue/nuxt', { css: false }],
+  ],
+  /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
